786d14640f658d8fe7dc02dbb06c7c1339ebf48d
Péter Burai <buraipeter93@gmail.com>
2017-05-10 16:53:48 +0200

    + first operable version of the project. The program waits an inputPath as root URL, an outPath in which it will write the map, and a depth size.

--- Try_Selenium/Selenium.py
+++ Try_Selenium/Selenium.py
@@ -1,29 +1,20 @@
 from selenium import webdriver
+from selenium.common.exceptions import *
 
-
-def explore(inputPath="http://www.ni.com/hu-hu.html", outputPath="map.txt", depth=3):
-    driver = webdriver.Chrome("C:\\Users\\Burai Péter\\Egyetem\\NI\\chromedriver.exe")
+def explore(inputPath="http://www.ni.com/hu-hu.html", outputPath="map.txt", depth=2):
+    driver = webdriver.Chrome("chromedriver.exe")
+    driver.set_page_load_timeout(30)
     driver.get(inputPath)
     myFile = open(outputPath, "w")
-    #myFile.write(inputPath+"\n")
-    #myFile.write("-------------------\n")
+
 
     map = {}
     lista = driver.find_elements_by_tag_name('a')
-    tmpList = []
-    tmpSubList = []
     lista2= []
     for ref in lista:
         r = ref.get_attribute('href')
-        #print(r)
         if (not(r is None or "javascript:void" in r or len(r) == 0)):
             lista2.append(ref.get_attribute('href'))
-            #myFile.write(r)
-            #myFile.write("\n")
-    #myFile.write("\n\n")
-    #for ref in lista2:
-        #print(ref.text+"  "+ref.get_attribute('href')+"\n")
-        #print(lista.index(ref))
     map[inputPath] = lista2
 
 
@@ -31,32 +22,25 @@ def explore(inputPath="http://www.ni.com/hu-hu.html", outputPath="map.txt", dept
         tmpList = []
         for ref in lista2:
             tmpList2 = []
-            #r = ref.get_attribute('href')
             print(ref)
-            #myFile.write(ref.get_attribute('href') + "\n")
-            #myFile.write("-------------------\n")
-            driver.quit()
-            driver = webdriver.Chrome("C:\\Users\\Burai Péter\\Egyetem\\NI\\chromedriver.exe")
             driver.get(ref)
             newUrl = driver.current_url
             if(newUrl in map.keys()):
-                print(newUrl+"  A ")
+                #print(newUrl+"  it has been already explored")
                 continue
             print(map.keys())
-            #driver.implicitly_wait(1000)
             tmpSubList = driver.find_elements_by_tag_name('a')
-            print("-----------------------------------------------------")
-            for t in tmpSubList:
-                print(t.get_attribute('href'))
-            print("-----------------------------------------------------")
             for subRef in tmpSubList:
-                sr = subRef.get_attribute('href')
+                try:
+                    sr = subRef.get_attribute('href')
+                except StaleElementReferenceException:
+                    print("Element is not available")
+                    continue
+
                 if ( not(sr is None or "javascript:void" in sr or len(sr) == 0)):
-                    tmpList.append(subRef.get_attribute('href'))
-                    tmpList2.append(subRef.get_attribute('href'))
-                    #myFile.write(sr)
-                    #myFile.write("\n")
-            #myFile.write("\n\n")
+                    tmpList.append(sr)
+                    tmpList2.append(sr)
+                    #print(sr+"\n")
             map[ref] = tmpList2
         lista2 = tmpList
 
